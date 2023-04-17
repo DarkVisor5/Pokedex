498983b0c3015bfcf37fd86a14322617d1e34678
DarkVisor5 <79510764+DarkVisor5@users.noreply.github.com>
2023-04-17 18:17:49 +0200

    The Beginning of the Pokedex
    Created the array pokemonList, and added some objects with name,height and type

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,8 @@
-alert('Hello world')
-
-let favoriteFood="carbonara pasta";
-document.write(favoriteFood);
+let pokemonList=[
+    {name:'Bulbasaur',  height:'0.7 m',  type:['grass','poison']},
+    {name:'Ivysaur',    height:'1 m',    type:['grass','poison']},
+    {name:'Venusaur',   height:'2 m',    type:['grass','poison']},
+    {name:'Charmender', height:'0.6 m',  type:'fire'},
+    {name:'Charmeleon', height:'1.1m',   type:'fire'},
+    {name:'Charizard',  height:'1.7m',   type:['fire','flying']}
+];
