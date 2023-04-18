1596f93f885152203381f1dcb9bb63bf1b2d6882
DarkVisor5 <79510764+DarkVisor5@users.noreply.github.com>
2023-04-18 17:27:53 +0200

    Removed variable
    Adjusted variables like old code, from many singular objects to one array

--- js/scripts.js
+++ js/scripts.js
@@ -1,22 +1,18 @@
 // Initialize an empty array to store the Pokemon list
-let pokemonList = [];
-
-// Define the individual Pokemon objects
-let pokemon1 = { name: 'Bulbasaur', height: 0.7, type: ['grass', 'poison'] };
-let pokemon2 = { name: 'Ivysaur', height: 1, type: ['grass', 'poison'] };
-let pokemon3 = { name: 'Venusaur', height: 2, type: ['grass', 'poison'] };
-let pokemon4 = { name: 'Charmender', height: 0.6, type: ['fire'] };
-let pokemon5 = { name: 'Charmeleon', height: 1.1, type: ['fire'] };
-let pokemon6 = { name: 'Charizard', height: 1.7, type: ['fire', 'flying'] };
-
-// Create an array containing all Pokemon objects
-let pokemonz = [pokemon1, pokemon2, pokemon3, pokemon4, pokemon5, pokemon6];
+let pokemonList = [;
+  { name: 'Bulbasaur', height: 0.7, type: ['grass', 'poison'] };
+  { name: 'Ivysaur', height: 1, type: ['grass', 'poison'] };
+  { name: 'Venusaur', height: 2, type: ['grass', 'poison'] };
+  { name: 'Charmender', height: 0.6, type: ['fire'] };
+  { name: 'Charmeleon', height: 1.1, type: ['fire'] };
+  { name: 'Charizard', height: 1.7, type: ['fire', 'flying'] };
+]
 
 // Get the pokedex-list element from the HTML
 let pokedexList = document.getElementById('pokedex-list');
 
-for (let i = 0; i < pokemonz.length; i++) {
-  pokemonList.push(pokemonz[i]);
+for (let i = 0; i < pokemonList.length; i++) {
+
   // Create a list item for the current Pokemon
   let listItem = document.createElement('li');
   listItem.innerHTML = 'Added ' + pokemonList[i].name + ' to the Pokedex. Its height is: ' + pokemonList[i].height;
