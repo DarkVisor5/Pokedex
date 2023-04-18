ccb00bdbe2f5e897218191c36f6440680cdd8c5f
DarkVisor5 <79510764+DarkVisor5@users.noreply.github.com>
2023-04-18 13:44:32 +0200

    Exercise Update
    Rewirtten the code, since the direction was to create a blank array, and not directly an array with the objects. I found that using a loop was easier instead of assigning and printing manually each pokemon to the array

--- js/scripts.js
+++ js/scripts.js
@@ -1,8 +1,15 @@
-let pokemonList=[
-    {name:'Bulbasaur',  height:'0.7 m',  type:['grass','poison']},
-    {name:'Ivysaur',    height:'1 m',    type:['grass','poison']},
-    {name:'Venusaur',   height:'2 m',    type:['grass','poison']},
-    {name:'Charmender', height:'0.6 m',  type:'fire'},
-    {name:'Charmeleon', height:'1.1m',   type:'fire'},
-    {name:'Charizard',  height:'1.7m',   type:['fire','flying']}
-];
+let pokemonList=[];
+
+let pokemon1= {name:'Bulbasaur',  height:'0.7 m',  type:['grass','poison']};
+let pokemon2= {name:'Ivysaur',    height:'1 m',    type:['grass','poison']};
+let pokemon3= {name:'Venusaur',   height:'2 m',    type:['grass','poison']};
+let pokemon4= {name:'Charmender', height:'0.6 m',  type:['fire']};
+let pokemon5= {name:'Charmeleon', height:'1.1m',   type:['fire']};
+let pokemon6= {name:'Charizard',  height:'1.7m',   type:['fire','flying']};
+
+let pokemonz=[pokemon1,pokemon2,pokemon3,pokemon4,pokemon5,pokemon6];
+
+for (let i=0;i<pokemonz.length;i++){
+    pokemonList.push(pokemonz[i]);
+    console.log('Added '+ pokemonz[i].name+' to the pokedex');
+}
