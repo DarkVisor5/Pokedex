eac90d9d392b2f9980ae719e8498376bb0684d22
DarkVisor5 <79510764+DarkVisor5@users.noreply.github.com>
2023-04-19 14:40:41 +0200

    For loop removed
    Changed the for loop with forEach

--- js/scripts.js
+++ js/scripts.js
@@ -1,24 +1,24 @@
 // Initialize an empty array to store the Pokemon list
-let pokemonList = [;
-  { name: 'Bulbasaur', height: 0.7, type: ['grass', 'poison'] };
-  { name: 'Ivysaur', height: 1, type: ['grass', 'poison'] };
-  { name: 'Venusaur', height: 2, type: ['grass', 'poison'] };
-  { name: 'Charmender', height: 0.6, type: ['fire'] };
-  { name: 'Charmeleon', height: 1.1, type: ['fire'] };
-  { name: 'Charizard', height: 1.7, type: ['fire', 'flying'] };
-]
+let pokemonList = [
+  { name: 'Bulbasaur', height: 0.7, type: ['grass', 'poison'] },
+  { name: 'Ivysaur', height: 1, type: ['grass', 'poison'] },
+  { name: 'Venusaur', height: 2, type: ['grass', 'poison'] },
+  { name: 'Charmender', height: 0.6, type: ['fire'] },
+  { name: 'Charmeleon', height: 1.1, type: ['fire'] },
+  { name: 'Charizard', height: 1.7, type: ['fire', 'flying'] },
+];
 
 // Get the pokedex-list element from the HTML
 let pokedexList = document.getElementById('pokedex-list');
 
-for (let i = 0; i < pokemonList.length; i++) {
-
+// Use forEach() to iterate over the Pokemon in the pokemonList array
+pokemonList.forEach(function(pokemon) {
   // Create a list item for the current Pokemon
   let listItem = document.createElement('li');
-  listItem.innerHTML = 'Added ' + pokemonList[i].name + ' to the Pokedex. Its height is: ' + pokemonList[i].height;
+  listItem.innerHTML = 'Added ' + pokemon.name + ' to the Pokedex. Its height is: ' + pokemon.height;
 
   // Check if the current Pokemon has a height greater than 1.0
-  if (pokemonList[i].height > 1.0) {
+  if (pokemon.height > 1.0) {
     // Create a span element to display the "That's a big boy!" text
     let bigText = document.createElement('span');
     bigText.innerHTML = " That's a big boy!";
@@ -29,4 +29,4 @@ for (let i = 0; i < pokemonList.length; i++) {
   }
   // Append the listItem to the pokedexList
   pokedexList.appendChild(listItem);
-}
+});
